7f5f2a59a284fe1b6701fb65d78ef5c5ea4f8214
Simon Maher <simonmaher8307@hotmail.com>
2021-11-18 15:54:50 +0200

    fixed bug in media query for mobile, resize img

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-const nav = document.querySelector('.primary-navigation')
-const navToggle = document.querySelector('.mobile-nav-toggle')
+const navSlide = () => {
+    const nav = document.querySelector('.primary-navigation')
+    const navToggle = document.querySelector('.mobile-nav-toggle')
+    const navLinks = document.querySelectorAll('.nav-links li')
 
-navToggle.addEventListener('click', () => {
-    const visibility = nav.getAttribute('data-visible')
+    
+    
+    
+    navToggle.addEventListener('click', () => {
+        const visibility = nav.getAttribute('data-visible')
+        
+        navLinks.forEach((link, index) =>{
+            if(link.style.animation) {
+                link.style.animation = ''
+            }else {
+                link.style.animation = `navLinkFade 0.5s ease forward ${index / 7 + 0.3}s`
+            }
+        }
+    )
+        if(visibility === 'false') {
+            nav.setAttribute('data-visible', true)
+            navToggle.setAttribute('aria-expanded', true)
+        }else {
+            nav.setAttribute('data-visible', false )
+            navToggle.setAttribute('aria-expanded', false)
+        }
 
-    if(visibility === 'false') {
-        nav.setAttribute('data-visible', true)
-        navToggle.setAttribute('aria-expanded', true)
-    }else {
-        nav.setAttribute('data-visible', false )
-        navToggle.setAttribute('aria-expanded', false)
-    }
 })
+}
+navSlide()
